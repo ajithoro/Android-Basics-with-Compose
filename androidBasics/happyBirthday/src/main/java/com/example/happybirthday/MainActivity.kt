20d1305b59fe5a136227ef1526adbab791e89fe0
Ajit Horo <ajithoro@gmail.com>
2025-08-30 01:13:24 +0530

    Complete happy birthday module

--- androidBasics/happyBirthday/src/main/java/com/example/happybirthday/MainActivity.kt
+++ androidBasics/happyBirthday/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,24 +29,53 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HappyBirthday("Horo", modifier = Modifier.padding(innerPadding))
-                }
+                HappyBirthday(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun HappyBirthday(name: String, modifier: Modifier = Modifier) {
-    Text("Hello $name")
+fun HappyBirthday(modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.androidparty),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5f
+        )
+        HappyBirthdayText(
+            modifier = Modifier
+                .padding(8.dp)
+                .align(alignment = Alignment.Center)
+        )
+    }
 }
 
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
 @Composable
-fun GreetingPreview() {
+fun HappyBirthdayText(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.happy_birthday_sam),
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = stringResource(R.string.from_emma),
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
 
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun HappyBirthdayPreview() {
+    HappyBirthday(modifier = Modifier)
 }
