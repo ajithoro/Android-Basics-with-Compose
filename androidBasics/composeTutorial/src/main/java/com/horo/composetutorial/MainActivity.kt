c0b665a550c6022c1ffee78c1d618499ecbe0af9
Ajit Horo <ajithoro@gmail.com>
2025-08-30 09:22:21 +0530

    Implements compose tutorial module

--- androidBasics/composeTutorial/src/main/java/com/horo/composetutorial/MainActivity.kt
+++ androidBasics/composeTutorial/src/main/java/com/horo/composetutorial/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.horo.composetutorial.ui.theme.AndroidBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,15 +29,48 @@ class MainActivity : ComponentActivity() {
             AndroidBasicsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     innerPadding
+                    ComposeTutorial(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    )
                 }
             }
         }
     }
 }
 
+@Composable
+fun ComposeTutorial(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null,
+        )
+        Text(
+            text = stringResource(R.string.text_title),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.text_desc1),
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.text_desc2),
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ComposeTutorialPreview() {
     AndroidBasicsTheme {
+        ComposeTutorial(modifier = Modifier)
     }
 }
