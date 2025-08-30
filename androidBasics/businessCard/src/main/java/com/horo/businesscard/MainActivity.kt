35487bd9307c5bccb0910f445739b3f6bf5ef84d
Ajit Horo <ajithoro@gmail.com>
2025-08-30 06:22:04 +0530

    Implements bussiness card

--- androidBasics/businessCard/src/main/java/com/horo/businesscard/MainActivity.kt
+++ androidBasics/businessCard/src/main/java/com/horo/businesscard/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.horo.businesscard.ui.theme.AndroidBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +35,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidBasicsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    BusinessCard(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +45,77 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BusinessCard(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize()
+                .background(Color.LightGray),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Image(
+                painter = painterResource(R.drawable.android_logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(200.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .background(Color.Black)
+            )
+            Text(
+                text = "Ajit Horo",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp),
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Android Developer Experience",
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+        }
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .background(Color.Magenta)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column {
+                Row(horizontalArrangement = Arrangement.Start) {
+                    Icon(
+                        imageVector = Icons.Filled.Call,
+                        contentDescription = null,
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+                    Text(
+                        text = "+91 9876543210",
+                    )
+                }
+                Row(horizontalArrangement = Arrangement.Start) {
+                    Icon(
+                        imageVector = Icons.Filled.Home,
+                        contentDescription = null,
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+                    Text(
+                        text = "+91 98765432109999",
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun BusinessCardPreview() {
     AndroidBasicsTheme {
-        Greeting("Android")
+        BusinessCard()
     }
 }
