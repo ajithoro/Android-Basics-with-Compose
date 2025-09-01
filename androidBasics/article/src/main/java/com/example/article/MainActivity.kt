2f41f5c49646e71fcfb62a1f5df920f5b9b051b7
Ajit Horo <ajithoro@gmail.com>
2025-09-01 14:15:56 +0530

    Implements finish screen

--- androidBasics/article/src/main/java/com/example/article/MainActivity.kt
+++ androidBasics/article/src/main/java/com/example/article/MainActivity.kt
@@ -4,33 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.article.ui.theme.ArticleTheme
 import com.example.article.ui.theme.Color1
 import com.example.article.ui.theme.Color2
@@ -57,42 +46,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun FinishScreen(message1: String,
-                 message2: String,
-                 modifier: Modifier = Modifier) {
-    Column(modifier = modifier,
-        verticalArrangement = Arrangement.Center) {
-        Image(
-            painter = painterResource(R.drawable.ic_task_completed),
-            contentDescription = null,
-            modifier = Modifier.align(
-                alignment = Alignment.CenterHorizontally
-            )
-        )
-        Text(
-            text = message1,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .align(
-                    Alignment.CenterHorizontally
-                )
-                .padding(
-                    top = 24.dp,
-                    bottom = 8.dp
-                )
-        )
-        Text(
-            text = message2,
-            fontSize = 16.sp,
-            modifier = Modifier.align(
-                Alignment.CenterHorizontally
-            )
-        )
     }
 }
 
@@ -201,11 +154,5 @@ fun GreetingPreview() {
 //                )
 //        )
 
-
-//        FinishScreen(
-//            message1 = stringResource(R.string.text_all_task),
-//            message2 = stringResource(R.string.text_nice_work),
-//            modifier = Modifier.fillMaxSize()
-//        )
     }
 }
--- androidBasics/finishScreen/src/main/java/com/horo/finishscreen/MainActivity.kt
+++ androidBasics/finishScreen/src/main/java/com/horo/finishscreen/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.horo.finishscreen.ui.theme.AndroidBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidBasicsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FinishScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +38,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FinishScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center) {
+        Image(
+            painter = painterResource(com.horo.android.R.drawable.ic_task_completed),
+            contentDescription = null,
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        )
+
+        Text(
+            text = stringResource(com.horo.android.R.string.text_all_task),
+            modifier = Modifier.padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(com.horo.android.R.string.text_nice_work),
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun FinishScreenPreview() {
     AndroidBasicsTheme {
-        Greeting("Android")
+        FinishScreen()
     }
 }
