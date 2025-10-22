c5b4a903a4765a45f0d6fcd367ddaaba4cc64d37
Ajit Horo <ajithoro@gmail.com>
2025-10-22 11:37:53 +0530

    Adds data layer and theming

--- androidBasics/woof/src/main/java/com/horo/woof/ui/theme/Type.kt
+++ androidBasics/woof/src/main/java/com/horo/woof/ui/theme/Type.kt
@@ -2,33 +2,39 @@ package com.horo.woof.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.horo.woof.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+val AbrilFatface = FontFamily(
+    Font(resId = R.font.abrilfatface_regular)
+)
+
+val Montserrat = FontFamily(
+    Font(resId = R.font.montserrat_regular),
+    Font(resId = R.font.montserrat_bold, weight = FontWeight.Bold)
+)
+val AppTypography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = AbrilFatface,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
-    */
 )
