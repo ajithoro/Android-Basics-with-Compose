150b4d41bb96bb571de3687594050afb23e2dd6b
Ajit Horo <ajithoro@gmail.com>
2025-10-24 15:32:48 +0530

    Adds hero list ui

--- androidBasics/superheroes/src/main/java/com/horo/superheroes/MainActivity.kt
+++ androidBasics/superheroes/src/main/java/com/horo/superheroes/MainActivity.kt
@@ -4,6 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.horo.superheroes.data.HeroesRepository
+import com.horo.superheroes.model.Hero
 import com.horo.superheroes.ui.theme.AndroidBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,8 +42,84 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidBasicsTheme {
-
+                HeroListScreen()
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HeroListScreen() {
+    Scaffold(topBar = { CenterAlignedTopAppBar({ Text(text = stringResource(R.string.app_name)) }) }) { paddingValues ->
+        Surface(modifier = Modifier.padding(paddingValues)) {
+            HeroList(heroes = HeroesRepository.heroes)
+        }
+    }
+}
+
+@Composable
+fun HeroList(heroes: List<Hero>, modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .padding(dimensionResource(R.dimen.padding_medium))
+    ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+        ) {
+            items(heroes) {
+                HeroListItem(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun HeroListItem(hero: Hero, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 2.dp
+        ), shape = MaterialTheme.shapes.medium
+    ) {
+        Row(modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = stringResource(hero.nameRes),
+                    style = MaterialTheme.typography.displayMedium
+                )
+                Text(
+                    text = stringResource(hero.descriptionRes),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+            Spacer(modifier = Modifier.size(dimensionResource(R.dimen.padding_medium)))
+            Box(modifier = Modifier.size(72.dp)) {
+                Image(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(MaterialTheme.shapes.small),
+                    painter = painterResource(hero.imageRes),
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    AndroidBasicsTheme {
+        HeroListScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DarkThemePreview() {
+    AndroidBasicsTheme(darkTheme = true) {
+        HeroListScreen()
     }
 }
