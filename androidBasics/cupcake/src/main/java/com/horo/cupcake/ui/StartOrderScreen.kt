310df207b903364d5f9260d06082f908a184ac10
Ajit Horo <ajithoro@gmail.com>
2025-11-04 23:48:00 +0530

    Adds partial ui for start order screen

--- androidBasics/cupcake/src/main/java/com/horo/cupcake/ui/StartOrderScreen.kt
+++ androidBasics/cupcake/src/main/java/com/horo/cupcake/ui/StartOrderScreen.kt
@@ -1,8 +1,53 @@
 package com.horo.cupcake.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.horo.cupcake.R
+import com.horo.cupcake.ui.theme.CupcakeTheme
 
 @Composable
-fun StartOrderScreen() {
+fun StartOrderScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
+            Image(
+                painter = painterResource(R.drawable.cupcake),
+                contentDescription = null
+            )
+            Text(
+                text = stringResource(R.string.order_cupcakes),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+
+        Column {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StartOrderScreenPreview() {
+    CupcakeTheme {
+        StartOrderScreen()
+    }
 }
