690e20a4e4f7b604f8182a79e1e24aa03b992c41
Ajit Horo <ajithoro@gmail.com>
2025-11-19 23:35:12 +0530

    Adds new lunch tray module with theme and launcher icons config

---
+++ androidBasics/lunchtray/src/main/java/com/horo/lunchtray/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.horo.lunchtray
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.horo.lunchtray.ui.theme.LunchTrayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LunchTrayTheme {
+
+            }
+        }
+    }
+}
