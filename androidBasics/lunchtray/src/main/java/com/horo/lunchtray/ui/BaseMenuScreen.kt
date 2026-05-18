1c1d6a7c1622c7ec96af610229a0babf02e67da0
Ajit Horo <ajithoro@gmail.com>
2026-05-18 23:03:54 +0530

    Adds base menu screen with menu items ui

---
+++ androidBasics/lunchtray/src/main/java/com/horo/lunchtray/ui/BaseMenuScreen.kt
@@ -0,0 +1,90 @@
+package com.horo.lunchtray.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.horo.lunchtray.R
+import com.horo.lunchtray.data.DataSource
+import com.horo.lunchtray.model.MenuItem
+
+@Composable
+fun BaseMenuScreen(
+    optionList: List<MenuItem>,
+    modifier: Modifier,
+) {
+
+    var selectedItemName by rememberSaveable { mutableStateOf("") }
+
+    Column(modifier = modifier.fillMaxSize()) {
+        optionList.forEach { item ->
+            MenuItem(
+                menuItem = item,
+                selectedItemName = selectedItemName,
+                onItemClick = { currentSelectedItemName ->
+                    selectedItemName = currentSelectedItemName
+                },
+                modifier = Modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun MenuItem(
+    menuItem: MenuItem,
+    selectedItemName: String,
+    onItemClick: (String) -> Unit,
+    modifier: Modifier,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .selectable(
+                selected = selectedItemName == menuItem.name,
+                onClick = { onItemClick(menuItem.name) }
+            ),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+    ) {
+        RadioButton(
+            selected = selectedItemName == menuItem.name,
+            onClick = { onItemClick(menuItem.name) },
+        )
+        Column {
+            Text(
+                text = menuItem.name,
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Text(
+                text = menuItem.description,
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = menuItem.getFormattedPrice(),
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BaseMenuScreenPreview() {
+    BaseMenuScreen(
+        optionList = DataSource.entreeMenuItems,
+        modifier = Modifier.fillMaxSize()
+    )
+}
